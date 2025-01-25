c59cc090bec42b5106af8385938ac47b2dd8edc4
itsbocchi <cristian.gonzalezjo@sansano.usm.cl>
2025-01-25 19:30:26 -0300

    Added flip to gargoyle and prefab

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,30 +6,39 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 4f;
     [SerializeField] private Transform[] waypoints;
+    private SpriteRenderer spriteRenderer;
     private int currentWaypointIndex = 0;
-    
-    public enum Direction
-    {
-        Left = 0,
-        Right = 1
-    }
+
+    public bool isInBubble = false;
 
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Patrol();
+
     }
 
     void Patrol()
     {
         Transform currentWaypoint = waypoints[currentWaypointIndex];
         transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, speed * Time.deltaTime);
-
+        
+        if (!isInBubble)
+        {
+            if (currentWaypoint.position.x < transform.position.x)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
 
         if (Vector3.Distance(transform.position, currentWaypoint.position) < 0.1f)
         {
